16c1dcf6499ff6ab6f45530bf212b3f2794c8b15
2inhee <raven023@naver.com>
2021-11-15 23:24:01 +0900

    댓글 수정 추가

--- board/urls.py
+++ board/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()), # 댓글 수정 페이지의 경로 추가
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()), # board/create_post 로 url 입력 시 PostCreate 클래스 사용
     path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/new_comment/', views.new_comment),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 
--- board/views.py
+++ board/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post, Category
+from django.shortcuts import get_object_or_404
+from .models import Post, Category, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
+
+# 댓글 수정
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return render(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else :
+            raise PermissionDenied
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else :
+            return redirect(post.get_absolute_url())
+    else :
+        raise PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -21,6 +51,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostCreate(LoginRequiredMixin, CreateView): # LoginRequiredMixin 추가하여 로그인했을때만 정상적으로 페이지 보이게 수정.
